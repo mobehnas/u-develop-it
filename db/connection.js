4ba5d6a9e9d25088050c29deeaf98e8b71f8c5a4
mohamed behnas <behna@DESKTOP-T1K4JBI.(none)>
2022-05-21 12:41:47 -0400

    Modularize the Application

---
+++ db/connection.js
@@ -0,0 +1,32 @@
+const mysql = require("mysql2");
+
+// Connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password 
+    password: 'MohamedBehnas5543@',
+    database: 'election'
+  });
+
+
+  module.exports = db;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  module.exports = db;
